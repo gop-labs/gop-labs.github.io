cfff1b2e7eb66ddb877d858a8426794d73472c71
mickgrz <mickgrz@gmail.com>
2015-01-10 21:52:18 +0100

    mgrz first version

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,10 @@
-console.log('This would be the main JS file.');
+
+function displayPage(page){
+ console.log('Display page "'+page+'"');
+ var pages = document.getElementsByName('page');
+ for (var i=0 ; i < pages.length ; i++){
+  pages[i].style.display = pages[i].id==page ? '' : 'none';
+ }
+}
+
+console.log('Main JS file is loaded.');
